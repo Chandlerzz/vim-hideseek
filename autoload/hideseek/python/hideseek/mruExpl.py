eec5f03727c610bb02112cb15143aeea65ca62fd
chandler xu <chandlerxu@localhost.localdomain>
2022-03-27 03:48:34 -0400

    test

---
+++ autoload/hideseek/python/hideseek/mruExpl.py
@@ -0,0 +1,30 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# import vim
+import re
+import os
+import os.path
+import fnmatch
+import time
+import locale
+from functools import wraps
+from .explorer import *
+
+class MruExplorer(Explorer):
+    def __init__(self):
+        self._cur_dir = ''
+        self._content = []
+        self._cmd_work_dir = ""
+
+    def getContent(self, *args, **kwargs):
+        content = ""
+        return content
+
+    def getStlCategory(self):
+        return "Mru"
+
+    def getStlCurDir(self):
+        curDir = ""
+        return curDir
